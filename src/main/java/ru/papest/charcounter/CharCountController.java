7a60a0f1a2f64a44afc39a709998b8d4df1eaa6e
Epap <peia@inbox.ru>
2023-11-12 19:57:52 +0300

    Add API Documentation with Restdocs http://localhost:8888/swagger-ui.html

--- src/main/java/ru/papest/charcounter/CharCountController.java
+++ src/main/java/ru/papest/charcounter/CharCountController.java
@@ -1,5 +1,8 @@
 package ru.papest.charcounter;
 
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.constraints.Size;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -11,9 +14,11 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Validated
+@Tag(name = "Char Counter Controller", description = "Контроллер для подсчета частоты символов в строке")
 public class CharCountController {
 
     @PostMapping(value = "/api/count/")
+    @Operation(summary = "Подсчет частоты символов", description = "Получение строки и подсчет частоты символов в ней")
     public ResponseEntity<String> postCount(@Size(max = 10) @RequestBody String text) {
         return ResponseEntity.ok(new FrequencyMap(text).getMapToString());
     }
